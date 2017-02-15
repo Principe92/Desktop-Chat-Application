db67deee408689943926d47e9f1eeb64c0a564d8
Princewill Okorie <pcokorie@gmail.com>
2017-02-15 02:18:43 -0600

    Added a few classes

---
+++ DesktopChatApplication/src/Init.java
@@ -0,0 +1,9 @@
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DesktopChatApplication/src/models/Client.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Client {
+	private String name;
+	private int id;
+
+}
