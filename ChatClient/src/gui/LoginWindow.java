f7c6fcd44872920219c3d06ec73e58647a2c9072
kaspersky43 <austinwyang94@gmail.com>
2017-04-29 17:10:08 -0500

    minor changes

--- ChatClient/src/gui/LoginWindow.java
+++ ChatClient/src/gui/LoginWindow.java
@@ -1,6 +1,7 @@
 package gui;
 
-import ChatLibrary.User;
+import model.AccountDB;
+import model.User;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,69 +13,61 @@ import java.awt.event.ActionListener;
  */
 public class LoginWindow {
     private JFrame frame;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+    private JButton createAccountButton;
+    private User user;
+    private AccountDB accounts;
 
-	/**
-	 * Constructs a new LoginWindow
-	 * @param user
-	 */
+    /**
+     * Constructs a new LoginWindow
+     *
+     * @param user
+     */
     public LoginWindow(final User user, AccountDB accounts) {
         this.user = user;
-		this.accounts = accounts;
-		frame = new JFrame();
-		frame.setLayout(new GridLayout(3,2));
-		
-		//create text fields, labels, and buttons
-		usernameField = new JTextField(25);
-		passwordField = new JPasswordField(25);
-		loginButton = new JButton("Login");
-		createAccountButton = new JButton("Create New Account");
-		
-		//add action listeners to buttons
-		loginButton.addActionListener(new ActionListener(){
-            ActionListener() {
-                @Override
-                public void actionPerformed (ActionEvent e){
-                    //CREATE ACCOUNT WINDOW
-                    //new CreateAccountWindow(user);
-                }
-            })
+        this.accounts = accounts;
+        frame = new JFrame();
+        frame.setLayout(new GridLayout(3, 2));
 
-                    //action listener needs to be implemented
-                    createAccountButton.addActionListener(new
+        //create text fields, labels, and buttons
+        usernameField = new JTextField(25);
+        passwordField = new JPasswordField(25);
+        loginButton = new JButton("Login");
+        createAccountButton = new JButton("Create New Account");
 
-            JLabel("Username"))
-
-                    //add components to frame
-                    frame.add(new
-
-            JLabel("Password"))
-                    frame.add(usernameField)
-                    frame.add(new
-
+        //add action listeners to buttons
+        loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-				boolean exists = accounts.checkCredentials(usernameField.getText(), passwordField.getPassword());
-				//if true, isLoggedIn = true
-				if (exists) {
-					user.setisLoggedIn(true);
-				//if false, System.out.println("Login attempt failed");
-			}
-            })
-                    frame.add(passwordField)
-                    frame.add(loginButton)
-                    frame.add(createAccountButton)
-                    frame.pack()
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-                    frame.setVisible(true)
-        }
-        private JTextField usernameField;
-	private JPasswordField passwordField;
-	private JButton loginButton;
-	private JButton createAccountButton;
-	//suppress "user is unused" warning - it is used in an action listener
-	@SuppressWarnings("unused")
-	private User user;
-	private AccountDB accounts;
-	//suppress "user is unused" warning - it is used in an action listener
-	@SuppressWarnings("unused")
+                //CREATE ACCOUNT WINDOW
+                //new CreateAccountWindow(user);
+            }
+        });
+
+        //action listener needs to be implemented
+        createAccountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+               /* boolean exists = accounts.checkCredentials(usernameField.getText(), passwordField.getPassword());
+                //if true, isLoggedIn = true
+                if (exists) {
+                    //if false, System.out.println("Login attempt failed");
+                }
+               // user.setisLoggedIn(true);*/
+            }
+        });
+
+        //add components to frame
+        frame.add(new JLabel("Password"));
+        frame.add(usernameField);
+
+        frame.add(passwordField);
+        frame.add(loginButton);
+        frame.add(createAccountButton);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
--- ChatClient/src/main/Main.java
+++ ChatClient/src/main/Main.java
@@ -1,69 +1,64 @@
 package main;
 
 import factory.AbstractFactory;
+import gui.JoinDialog;
+import gui.LoginPanel;
 import model.ChatDb;
 import model.ChatManager;
 import type.ILogger;
 import type.ISocketProtocol;
 
-import gui.JoinDialog;
-import gui.LoginPanel;
-import main.App;
-import main.Client;
-import main.Server;
-
+import javax.swing.*;
 import java.io.IOException;
 
-import javax.swing.JOptionPane;
-
 public class Main {
-	
-	private static final Main instance = new Main();
-	private JoinDialog joinDialog;
-	private App app;
-	private LoginPanel loginPanel;
-	private Client client;
-	private Server server;
-	
 
-    public static void main(String[] args) throws IOException {   	
+    private static final Main instance = new Main();
+    private JoinDialog joinDialog;
+    private App app;
+    private LoginPanel loginPanel;
+    private Client client;
+    private Server server;
+
+
+    private Main() {
         ILogger logger = AbstractFactory.getLogger();
         ISocketProtocol protocol = AbstractFactory.getProtocol();
-        
-        Main.getInstance().startClient();
+
+        app = new App(logger, protocol, ChatDb.Instance(logger), ChatManager.instance());
+    }
+
+    public static void main(String[] args) throws IOException {
+        ILogger logger = AbstractFactory.getLogger();
+        ISocketProtocol protocol = AbstractFactory.getProtocol();
+
+        //  Main.getInstance().startClient();
 
         //If logged in then proceed to the app
         new App(logger, protocol, ChatDb.Instance(logger), ChatManager.instance());
     }
-    
-    private Main() {
-    	ILogger logger = AbstractFactory.getLogger();
-        ISocketProtocol protocol = AbstractFactory.getProtocol();
-        
-        app = new App(logger, protocol, ChatDb.Instance(logger), ChatManager.instance());
-    }
-    
+
     public static Main getInstance() {
-    	return instance;
-    }
-    
-    
-    private void startClient() {
-    	openLogin();
-    }
-    
-    private void openLogin(){
-    	//Connect with socket
-    	try {
-    		loginPanel = new LoginPanel();
-    		//loginPanel.setVisible(true);
-    	} catch (Exception e){
-    		JOptionPane.showMessageDialog(null, "cannot connect to server");
-    	}
+        return instance;
     }
 
-	public void requestLogin(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+
+    private void startClient() {
+        openLogin();
+    }
+
+    private void openLogin() {
+        //Connect with socket
+        try {
+            loginPanel = new LoginPanel();
+            //loginPanel.setVisible(true);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "cannot connect to server");
+        }
+    }
+
+    public void requestLogin(String string) {
+        // TODO Auto-generated method stub
+
+    }
 }
