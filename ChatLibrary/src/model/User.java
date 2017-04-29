1185eadaf4200ddd8257489cdc4b08c3dcfd3e4f
mhogan26 <mhogan26@slu.edu>
2017-04-29 18:38:14 -0500

    removed id

--- ChatLibrary/src/model/User.java
+++ ChatLibrary/src/model/User.java
@@ -1,24 +1,13 @@
 package model;
 
 public class User {
-    private int id;
 	private String pwd;
 	private String name;
 	private String nick;
 	private String email;
     private boolean isLoggedIn;
 
-    public User(int id) {
-        this.id = id;
-		this.pwd = "";
-        this.name = String.format("User %d", id);
-        this.nick = "";
-		this.email = "";
-        this.isLoggedIn = false;
-	}
-
-    public User(int id, String pwd, String name, String nick, String email) {
-        this.id = id;
+    public User(String pwd, String name, String nick, String email) {
 		this.pwd = pwd;
 		this.name = name;
 		this.nick = nick;
@@ -27,19 +16,10 @@ public class User {
 	}
 
     public User(String[] info) {
-        id = Integer.parseInt(info[0]);
 		pwd = info[1];
 		name = info[2];
 		nick = info[3];
 		email = info[4];
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public String getPwd() {
@@ -83,7 +63,6 @@ public class User {
     }
 
 	public void setAll(String[] tmp) {
-		id = Integer.parseInt(tmp[0]);
 		pwd = tmp[1];
 		name = tmp[2];
 		nick = tmp[3];
