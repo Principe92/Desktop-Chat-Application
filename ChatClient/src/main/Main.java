0f36503d2cefd93710dcf942762922df5f3d1806
kaspersky43 <austinwyang94@gmail.com>
2017-04-29 14:47:24 -0500

    designing login client that pops up b4 the main App

--- ChatClient/src/main/Main.java
+++ ChatClient/src/main/Main.java
@@ -6,15 +6,45 @@ import model.ChatManager;
 import type.ILogger;
 import type.ISocketProtocol;
 
+import gui.JoinDialog;
+import gui.LoginPanel;
+import main.App;
+import main.Client;
+import main.Server;
+
 import java.io.IOException;
 
 public class Main {
+	
+	private static final Main instance = new Main();
+	private JoinDialog joinDialog;
+	private App app;
+	private LoginPanel loginPanel;
+	private Client client;
+	private Server server;
+	
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {   	
         ILogger logger = AbstractFactory.getLogger();
         ISocketProtocol protocol = AbstractFactory.getProtocol();
-
+        
+        Main.getInstance().startClient();
+        
+        
+        
+        //If logged in then proceed to the app
         new App(logger, protocol, ChatDb.Instance(logger), ChatManager.instance());
     }
-
+    
+    private static Main getInstance() {
+    	return instance;
+    }
+    
+    private void startClient() {
+    	openLogin();
+    }
+    
+    private void openLogin(){
+    	//Connect with socket 
+    }
 }
